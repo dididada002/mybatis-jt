5a18393805b8f23d004c01843230fec05b668139
jingteng <jingteng@hejun.com>
2018-11-17 18:49:21 +0800

    新建工程

---
+++ src/main/java/com/dididada/jt/JtApplication.java
@@ -0,0 +1,12 @@
+package com.dididada.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtApplication.class, args);
+    }
+}
